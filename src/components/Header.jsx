a4302cf1a5a50bf0ec20ec9b11e87d3202bbc894
MattMatt-aka-Matt <78562870+MattMatt-aka-Matt@users.noreply.github.com>
2024-04-08 17:22:02 +0200

    format fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,33 @@
+
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav';
 function Header() {
     return (
-      <header >
-        <img src="https://cdn.pixabay.com/photo/2018/03/26/14/18/man-3262834_960_720.png" alt="un mexicain avec une guitard lol"></img>
-        <nav>
-            <ul>
-                <li><a href="/">accueil</a></li>
-            </ul>
-        </nav>
-      </header>
+      <Container>
+      <Navbar expand="lg" className="bg-body-tertiary">
+          <Navbar.Brand href="#"><img src="https://cdn.pixabay.com/photo/2018/03/26/14/18/man-3262834_960_720.png" alt="un mexicain avec une guitard lol"/></Navbar.Brand>
+          <Nav>
+            <Nav.Link href="#accueil">Acuueil</Nav.Link>
+          </Nav>
+      </Navbar>
+    </Container>
     );
   }
 
   export default Header;
+
+
+
+
+
+// export default ContainerOutsideExample;
+
+// <header >
+// <img src="https://cdn.pixabay.com/photo/2018/03/26/14/18/man-3262834_960_720.png" alt="un mexicain avec une guitard lol"></img>
+// <nav>
+//     <ul>
+//         <li><a href="/">accueil</a></li>
+//     </ul>
+// </nav>
+// </header>
