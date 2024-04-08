85600216be726c3cd361edf0bdc65ea2e54fa0b7
MattMatt-aka-Matt <78562870+MattMatt-aka-Matt@users.noreply.github.com>
2024-04-08 14:45:13 +0200

    activité 4

---
+++ src/components/header.jsx
@@ -0,0 +1,16 @@
+
+
+function Header() {
+    return (
+      <div >
+        <img src="https://cdn.pixabay.com/photo/2018/03/26/14/18/man-3262834_960_720.png" alt="un mexicain avec une guitard lol"></img>
+        <nav>
+            <lu>
+                <li><a href="/">accueil</a></li>
+            </lu>
+        </nav>
+      </div>
+    );
+  }
+
+  export default Header;
