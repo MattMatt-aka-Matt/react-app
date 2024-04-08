f1a5b3f67a99d5ff92197c6c08a2697ee2fe7cb8
MattMatt-aka-Matt <78562870+MattMatt-aka-Matt@users.noreply.github.com>
2024-04-08 14:49:37 +0200

    activité 4.1

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,13 +1,11 @@
-
-
 function Header() {
     return (
       <div >
         <img src="https://cdn.pixabay.com/photo/2018/03/26/14/18/man-3262834_960_720.png" alt="un mexicain avec une guitard lol"></img>
         <nav>
-            <lu>
+            <ul>
                 <li><a href="/">accueil</a></li>
-            </lu>
+            </ul>
         </nav>
       </div>
     );
