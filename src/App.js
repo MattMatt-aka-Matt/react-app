4f6697da75f25736e7bbe451fe6e1216aa53a894
MattMatt-aka-Matt <78562870+MattMatt-aka-Matt@users.noreply.github.com>
2024-04-08 15:49:24 +0200

    tet

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 
 import './App.css';
-import Header from './components/header';
-import Footer from './components/footer';
+import Header from './components/Header';
+import Footer from './components/Footer';
 import Plat from './components/Plat';
 
 function App() {
   return (
     <div >
       <Header/>
-      <Plat name="Tacos à l’unité" image="https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg" price="3"/>
-      <Plat name="Enchiladas" image="https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg" price="12"/>
-      <Plat name="Mole poblano" image="https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg" price="15"/>
+        <main>
+        <Plat name="Tacos à l’unité" image="https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg" price="3"/>
+        <Plat name="Enchiladas" image="https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg" price="12"/>
+        <Plat name="Mole poblano" image="https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg" price="15"/>
+        </main>
       <Footer/>
     </div>
   );
