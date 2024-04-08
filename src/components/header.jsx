46ed577b79ec11bde7bb3877bcbb9b3a0c39a800
MattMatt-aka-Matt <78562870+MattMatt-aka-Matt@users.noreply.github.com>
2024-04-08 14:50:32 +0200

    Update header.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,13 +1,13 @@
 function Header() {
     return (
-      <div >
+      <header >
         <img src="https://cdn.pixabay.com/photo/2018/03/26/14/18/man-3262834_960_720.png" alt="un mexicain avec une guitard lol"></img>
         <nav>
             <ul>
                 <li><a href="/">accueil</a></li>
             </ul>
         </nav>
-      </div>
+      </header>
     );
   }
 
