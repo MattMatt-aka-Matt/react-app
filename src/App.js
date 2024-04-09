2217bb0fcb244d5015254574b8fddb3ca1577bb9
MattMatt-aka-Matt <78562870+MattMatt-aka-Matt@users.noreply.github.com>
2024-04-09 11:42:48 +0200

    col xs to col

--- src/App.js
+++ src/App.js
@@ -13,21 +13,21 @@ function App() {
       <main>
         <Container>
           <Row>
-            <Col xs >
+            <Col>
               <Dish
                 name="Tacos à l’unité"
                 image="https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg"
                 price="3"
               />
             </Col>
-            <Col xs >
+            <Col>
               <Dish
                 name="Enchiladas"
                 image="https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg"
                 price="12"
               />
             </Col>
-            <Col xs >
+            <Col>
               <Dish
                 name="Mole poblano"
                 image="https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg"
