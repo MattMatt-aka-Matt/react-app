6ee6ce55dc8caa762994889be6f0d0e95c68e16a
MattMatt-aka-Matt <78562870+MattMatt-aka-Matt@users.noreply.github.com>
2024-04-10 10:13:57 +0200

    logo link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,34 +2,43 @@ import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import "../assets/styles/Header.css";
-import { NavLink } from 'react-router-dom';
-import logo from '../assets/images/logo.webp';
+import { NavLink } from "react-router-dom";
+import logo from "../assets/images/logo.webp";
 
 function Header() {
   return (
     <header>
       <Navbar expand="lg" className="bg-body-tertiary">
         <Container>
-          <Navbar.Brand href="#home">
-            <img 
+        <NavLink className="nav-link" to="/">
+          <Navbar.Brand to="/">
+            <img
               src={logo}
               alt="un mexicain avec une guitard lol"
               className="d-inline-block align-top"
             />
           </Navbar.Brand>
+          </NavLink>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse
             id="basic-navbar-nav"
             className="justify-content-end"
           >
             <Nav>
-              <NavLink className="nav-link" to="/">Accueil</NavLink>
-              <NavLink className="nav-link" to="/about" activeClassName="active">a propos</NavLink>
+              <NavLink className="nav-link" to="/">
+                Accueil
+              </NavLink>
+              <NavLink
+                className="nav-link"
+                to="/about"
+              >
+                a propos
+              </NavLink>
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
-      </header>
+    </header>
   );
 }
 
