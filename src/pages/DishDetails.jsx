5a555a50d0bac95e9c0f0969f44b3fe99dc46923
MattMatt-aka-Matt <78562870+MattMatt-aka-Matt@users.noreply.github.com>
2024-04-12 10:07:36 +0200

    add cart

--- src/pages/DishDetails.jsx
+++ src/pages/DishDetails.jsx
@@ -1,35 +1,32 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { Container, Row, Col, Button } from 'react-bootstrap';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Container, Row, Col, Button } from "react-bootstrap";
 import dishesDatas from "../datas/dishes.json";
 
-function DishDetails() {
-  
+function DishDetails({ addToCart }) {
   const { slug } = useParams();
-  const[dish, setDish] = useState(null);
+  const [dish, setDish] = useState(null);
 
-  useEffect(() =>{
-  const currentDish = dishesDatas.find((dish) => dish.slug === slug);
-  setDish(currentDish);
-  }, [slug])
+  useEffect(() => {
+    const currentDish = dishesDatas.find((dish) => dish.slug === slug);
+    setDish(currentDish);
+  }, [slug]);
 
   return (
     <Container>
       {dish ? (
-      <Row>
-        <Col>
-        <img src={dish.image} alt={dish.name} /> 
-        </Col>
-        <Col> 
-        <h1>{dish.name}</h1>
-        <p>{dish.description}</p>
-        <p>{dish.price}€</p>
-        
-        <Button variant="primary" >
-        bouton 
-        </Button>
-        </Col>
-      </Row>
+        <Row>
+          <Col>
+            <img src={dish.image} alt={dish.name} />
+          </Col>
+          <Col>
+            <h1>{dish.name}</h1>
+            <p>{dish.description}</p>
+            <p>{dish.price}€</p>
+            <Button onClick={() => addToCart(dish)}>Ajouter au panier</Button>
+            <Button variant="primary">bouton</Button>
+          </Col>
+        </Row>
       ) : (
         <p>chargement en cours</p>
       )}
